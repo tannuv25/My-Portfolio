e05b10ee29d32beeb6f0996b3e1cda714332ff91
tannuv <tannu.fourbrick@gmail.com>
2025-11-10 18:20:54 +0530

    new

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,13 +2,13 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { HeroSectionOne } from "../components/Hero";
-import Testimonials from "../components/Testimonial";
+// import Testimonials from "../components/Testimonial";
 
 export default function HomePage() {
     return <>
         <div className="overflow-hidden">    <Navbar />
             <HeroSectionOne />
-            <Testimonials />
+            {/* <Testimonials /> */}
 
             <Footer /></div>
     </>
